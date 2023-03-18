22dd02b51ebf0484ef840774634b75ee60deeee0
ptrv-dev <petrov99119@gmail.com>
2023-03-18 15:44:15 +0200

    Testimonials Section - responsive (all devices)

--- src/js/app.js
+++ src/js/app.js
@@ -5,3 +5,31 @@ headerMenuButton.addEventListener('click', () => {
   headerMenuButton.classList.toggle('header__menu_active');
   headerNavigation.classList.toggle('nav_active');
 });
+
+const sliderButtons = {
+  previous: document.querySelector('.testimonials-slider__button_prev'),
+  next: document.querySelector('.testimonials-slider__button_next'),
+};
+const slides = document.querySelectorAll('.testimonial');
+let currentSlide = 0;
+
+console.log(slides.length);
+
+const updateSlide = () => {
+  slides.forEach((slide) => slide.classList.remove('testimonial_active'));
+  slides[currentSlide].classList.add('testimonial_active');
+};
+
+const handleNext = () => {
+  if (currentSlide >= slides.length - 1) currentSlide = 0;
+  else currentSlide++;
+  updateSlide();
+};
+const handlePrevious = () => {
+  if (currentSlide <= 0) currentSlide = slides.length - 1;
+  else currentSlide--;
+  updateSlide();
+};
+
+sliderButtons.previous.addEventListener('click', handlePrevious);
+sliderButtons.next.addEventListener('click', handleNext);
